9e260a03303bbd4926b21035d6302e2901e23cfc
nallg00d <nick.allgood@gmail.com>
2018-01-01 20:31:19 -0500

    Updated driver.py runner and timer

--- wsnsims/conductor/driver.py
+++ wsnsims/conductor/driver.py
@@ -88,8 +88,13 @@ def run_loaf(parameters):
     env.comms_range = parameters.radio_range
 
     loaf_sim = LOAF(env)
-    runner = loaf_sim.run()
 
+    print(
+        "Starting LOAF at {}".format(datetime.datetime.now().isoformat()))
+    print("Using {}".format(parameters))
+    start = time.time()
+    runner = loaf_sim.run()
+    
     results = Results(runner.maximum_communication_delay(),
                       runner.energy_balance(),
                       0.,
