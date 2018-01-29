014e1bdb0c883c1ca13d2bf11d79ed0b2405d540
nallg00d <nick.allgood@gmail.com>
2018-01-29 17:17:52 -0500

    added spot in segment to set volume manually

--- wsnsims/loaf/segment.py
+++ wsnsims/loaf/segment.py
@@ -5,7 +5,16 @@ class LoafSegment(Segment):
     def __init__(self, nd):
         super(LoafSegment, self).__init__(nd)
         self.cell_id = -1
+        self.segment_volume = 0
 
+    @property
+    def volume(self):
+        return self.segment_volume
+
+    @volume.setter
+    def volume(self, volume):
+        self.segment_volume = volume
+        
     def __str__(self):
         return "LOAF Segment {}".format(self.segment_id)
 
